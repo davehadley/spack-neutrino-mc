fd2f77555cdb8c3483e06ee4cd3bc7e082cfffcf
David Hadley <davehadley@users.noreply.github.com>
2021-03-12 17:28:45 +0000

    add deps.

---
+++ setup.sh
@@ -0,0 +1,2 @@
+
+. spack/share/spack/setup-env.sh
--- spack-neutrino-mc/packages/genie/package.py
+++ spack-neutrino-mc/packages/genie/package.py
@@ -1,46 +1,23 @@
-# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install generator
-#
-# You can edit this file again by typing:
-#
-#     spack edit generator
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
-from spack import *
+from spack import AutotoolsPackage, depends_on, version
 
 
-class Generator(AutotoolsPackage):
-    """FIXME: Put a proper description of your package here."""
+class GenieGenerator(AutotoolsPackage):
+    """Genie Generator."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
-    url      = "https://github.com/GENIE-MC/Generator/archive/R-3_00_06.tar.gz"
+    homepage = "https://www.genie-mc.org"
+    url = "https://github.com/GENIE-MC/Generator/archive/R-3_00_06.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    #a amaintainers = ['github_user1', 'github_user2']
+    amaintainers = [
+        "davehadley",
+    ]
 
-    version('3_00_06', sha256='ab56ea85d0c1d09029254365bfe75a1427effa717389753b9e0c1b6c2eaa5eaf')
+    version(
+        "3_00_06",
+        sha256="ab56ea85d0c1d09029254365bfe75a1427effa717389753b9e0c1b6c2eaa5eaf",
+    )
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    depends_on("root", "pythia6", "lahpdf", "log4cpp", "libxml2", "gsl", "geant4")
 
     def configure_args(self):
-        # FIXME: Add arguments other than --prefix
-        # FIXME: If not needed delete this function
         args = []
         return args

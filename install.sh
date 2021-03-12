6207eb6846597705430a3fb5a764ca79eb5fc307
David Hadley <davehadley@users.noreply.github.com>
2021-03-12 17:59:07 +0000

    First version that runs.

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-#ln -s $(pwd)/spack-neutrino-mc/ spack/var/spack/repos/spack-neutrino-mc
-source setup.sh && spack repo add spack-neutrino-mc
+source setup.sh && spack repo add neutrino-mc
 
--- neutrino-mc/packages/genie/package.py
+++ neutrino-mc/packages/genie/package.py
@@ -1,4 +1,6 @@
-from spack import AutotoolsPackage, depends_on, version
+# from spack import AutotoolsPackage, depends_on, version
+from spack.build_systems.autotools import AutotoolsPackage
+from spack.directives import depends_on, version
 
 
 class Genie(AutotoolsPackage):
@@ -16,7 +18,13 @@ class Genie(AutotoolsPackage):
         sha256="ab56ea85d0c1d09029254365bfe75a1427effa717389753b9e0c1b6c2eaa5eaf",
     )
 
-    depends_on("root", "pythia6", "lahpdf", "log4cpp", "libxml2", "gsl", "geant4")
+    depends_on("root")
+    depends_on("pythia6")
+    depends_on("lhapdf")
+    depends_on("log4cpp")
+    depends_on("libxml2")
+    depends_on("gsl")
+    depends_on("geant4")
 
     def configure_args(self):
         args = []

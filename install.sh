9dc53f45947f8be7bb263855f69edd2a256d25e9
David Hadley <davehadley@users.noreply.github.com>
2021-03-20 19:07:50 +0000

    Add E4S build cache.

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@
 
 source setup.sh && spack repo add neutrino-mc
 
+# add E4S build cache
+spack mirror add E4S https://cache.e4s.io \
+&& wget https://oaciss.uoregon.edu/e4s/e4s.pub \
+&& spack gpg trust e4s.pub \
+&& rm e4s.pub

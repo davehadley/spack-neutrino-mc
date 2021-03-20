79edc18bdd620087449429f21eaf2b698deaa65b
David Hadley <davehadley@users.noreply.github.com>
2021-03-20 17:58:47 +0000

    Fix problems with install failing on genie 2.10.10

--- neutrino-mc/packages/genie/package.py
+++ neutrino-mc/packages/genie/package.py
@@ -5,6 +5,8 @@ from spack.directives import depends_on, patch, variant, version
 from spack.package import Package
 from spack.util.executable import Executable
 
+# from llnl.util.filesystem import FileFilter
+
 
 class Genie(Package):  # Genie doesn"t use Autotools
     """Genie Neutrino MC Generator."""
@@ -124,13 +126,14 @@ class Genie(Package):  # Genie doesn"t use Autotools
 
     depends_on("root+pythia6")
     depends_on("pythia6")
-    depends_on("lhapdf")
+    depends_on("lhapdf", when="@3:")
+    depends_on("lhapdf5", when="@:2")
     depends_on("log4cpp")
     depends_on("libxml2")
     depends_on("gsl")
     depends_on("geant4")
 
-    patch("genie_make_files.patch", level=0)
+    patch("genie_make_files.patch", level=0, when="@2.12:")
 
     # Flags for GENIE"s optional but disabled by default features
     variant(
@@ -166,7 +169,7 @@ class Genie(Package):  # Genie doesn"t use Autotools
         description="Enable GENIE neutrino masterclass app",
     )
 
-    phases = ["configure", "build", "install"]
+    phases = ["edit", "configure", "build", "install"]
 
     def configure(self, spec, prefix):
         configure = Executable("./configure")
@@ -178,14 +181,22 @@ class Genie(Package):  # Genie doesn"t use Autotools
             "--enable-lhapdf6",
             "--prefix=%s" % prefix,
             "--with-compiler=%s" % os.environ["CC"],
-            "--with-lhapdf6-inc=%s" % spec["lhapdf"].prefix.include,
-            "--with-lhapdf6-lib=%s" % spec["lhapdf"].prefix.lib,
             "--with-libxml2-inc=%s%slibxml2" % (spec["libxml2"].prefix.include, os.sep),
             "--with-libxml2-lib=%s" % spec["libxml2"].prefix.lib,
             "--with-log4cpp-inc=%s" % spec["log4cpp"].prefix.include,
             "--with-log4cpp-lib=%s" % spec["log4cpp"].prefix.lib,
             "--with-pythia6-lib=%s" % spec["pythia6"].prefix.lib,
         ]
+        if self.spec.satisfies("@:2"):
+            args += [
+                "--with-lhapdf-inc=%s" % spec["lhapdf5"].prefix.include,
+                "--with-lhapdf-lib=%s" % spec["lhapdf5"].prefix.lib,
+            ]
+        else:
+            args += [
+                "--with-lhapdf6-inc=%s" % spec["lhapdf"].prefix.include,
+                "--with-lhapdf6-lib=%s" % spec["lhapdf"].prefix.lib,
+            ]
         if "+vleextension" in self.spec:
             args += ["--enable-vle-extension"]
         if "+validationtools" in self.spec:
@@ -204,23 +215,26 @@ class Genie(Package):  # Genie doesn"t use Autotools
             args += ["--enable-masterclass"]
         return args
 
+    def edit(self, spec, prefix):
+        # makefile = FileFilter("src/make/Make.include")
+        # makefile.filter(r"CC = gcc",  "CC = dummygcccommand")
+        # makefile.filter(r"CXX = g\+\+", "CXX = dummygxxcommand")
+        # makefile.filter(r"LD  = g\+\+", "LD = dummyldcommand")
+        pass
+
     def build(self, spec, prefix):
         # make is injected by build_environment:
         # https://spack.readthedocs.io/en/latest/spack.html#module-spack.build_environment
         # apparently this is considered more readable
-        make()  # noqa
+        self._make(parallel=spec.satisfies("@3:"))
 
     def install(self, spec, prefix):
         # GENIE make install support parallel jobs
-        make("install", parallel=False)  # noqa
+        self._make("install", parallel=False)
         # GENIE requires these files to be present at runtime, but doesn"t install them
         # so we must install them ourselves
         install_tree("config", os.sep.join((prefix, "config")))  # noqa
         install_tree("data", os.sep.join((prefix, "data")))  # noqa
-
-    def makedirs(self, path):
-        if not os.path.exists(path):
-            os.makedirs(path)
 
     def setup_build_environment(self, env):
         env.set("GENIE", self.stage.source_path)
@@ -229,3 +243,18 @@ class Genie(Package):  # Genie doesn"t use Autotools
     def setup_run_environment(self, env):
         env.set("GENIE", self.prefix)
         return super().setup_run_environment(env)
+
+    def _make(self, *args, parallel=True):
+        # make is injected by build_environment:
+        # https://spack.readthedocs.io/en/latest/spack.html#module-spack.build_environment
+        # apparently this is considered more readable
+        args = list(self._make_args) + list(args)
+        make(*args, parallel=parallel)  # noqa
+
+    @property
+    def _make_args(self):
+        return [
+            "CC=c++",
+            "CXX=c++",
+            "LD=c++",
+        ]
